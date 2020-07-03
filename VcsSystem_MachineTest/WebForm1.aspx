bae9712abff33aa2a08564f1cb4d5cfe62e00768
Naveen <Naveen@NAVEEN-PC>
2020-07-03 21:04:56 +0530

    Add project files.

---
+++ VcsSystem_MachineTest/WebForm1.aspx
@@ -0,0 +1,104 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="VcsSystem_MachineTest.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="div-tab" style="position: relative; z-index: auto; top: -6px; left: 10px; width: 1180px;" tabindex="2">
+        <div id="basic-details" style="width: 420px">
+        <asp:Label ID="lblName" runat="server" Text="Name:"></asp:Label>
+        <asp:TextBox ID="txtName" runat="server"></asp:TextBox><br/>
+        <asp:Label ID="lblEmail" runat="server" Text="Email:"></asp:Label>
+        <asp:TextBox ID="txtEmail" runat="server" required="required" ></asp:TextBox><br/>
+        <asp:Label ID="lblCalender" runat="server" Text="Date of Birth:"></asp:Label>
+            <asp:TextBox ID="txtcalnder" runat="server"></asp:TextBox><br/><br/>
+            <asp:Label ID="lblImage" runat="server" Text="Image:"></asp:Label><br/>
+        <asp:FileUpload ID="fuImage" runat="server" /><br/>
+            </div>
+        <div id="image-tab">
+            <asp:Image ID="Image1" ImageUrl="" EnableViewState="true"
+               Visible="true"  runat="server" Height="188px" Width="183px" />
+            </div>
+        </div>
+        <asp:Button ID="btnSubmit" runat="server" Text="Sumit" OnClick="btnSubmit_Click" /><br/>
+        <br/>
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+       
+        <div id="gridView">
+        <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" AutoGenerateColumns="false" 
+            GridLines="None" DataKeyNames="Eid" OnPageIndexChanging="GridView1_PageIndexChanging" 
+            OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowDeleting="GridView1_RowDeleting" 
+            OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" allowpaging="true" >
+            <AlternatingRowStyle BackColor="White"  />
+            <pagersettings mode="Numeric"
+                        position="Bottom"           
+                    pagebuttoncount="10"/>
+                      
+                <pagerstyle backcolor="LightBlue"
+                  height="30px"
+                  verticalalign="Bottom"
+                  horizontalalign="Center"/>
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+             <Columns>  
+                        <asp:BoundField DataField="Eid" HeaderText="S.No." />  
+                        <asp:BoundField DataField="Ename" HeaderText="Name" />  
+                        <asp:BoundField DataField="Email" HeaderText="email" />
+                        <asp:BoundField DataField="Dob" HeaderText="DOB" />  
+                        <asp:BoundField DataField="pic" HeaderText="Image_URL" Visible="true"/>  
+                        <asp:CommandField ShowEditButton="true" />  
+                        <asp:CommandField ShowDeleteButton="true" /> </Columns>
+             <EmptyDataTemplate>
+        <div align="center">No records found.</div>
+    </EmptyDataTemplate>
+        </asp:GridView>
+            </div>
+    </form>
+    <style>
+        .div-tab {
+            flex:2;
+        display:flex;
+        
+        }
+    </style>
+   <script>
+       <%--var email = document.getElementById('TextBox2');
+       ValidEmail(email.innerText);
+
+       function ValidateDate(date) {
+           if (date < Date.now()) {
+               return true;
+           } else {
+               return false;
+           }
+       }--%>
+       //function ValidEmail(emailid) {
+       //    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)) {
+       //        return true;
+       //    }
+       //    alert("You have entered an invalid email address!")
+       //    return false;
+       //}
+       var grdv = document.querySelectorAll('#GridView1');
+       grdv.forEach(e=> {
+           e.addEventListener('click', e=> {
+               e=> {
+                   var image = document.getElementById('#Image1');
+                   image.setAttribute('ImageUrl', "E:\\VS_Code\\VcsSystem_MachineTest\\VcsSystem_MachineTest\\Images\\" + e.cells[4].innerText);
+               }});
+           });
+   </script>
+</body>
+</html>
---
+++ VcsSystem_MachineTest/WebForm1.aspx.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+
+namespace VcsSystem_MachineTest
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string imagepath = @"E:\VS_Code\VcsSystem_MachineTest\VcsSystem_MachineTest\Images\";
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings[
+               "connection"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                binddata();
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            Employee emp = new Employee();
+          emp= GetDetails(emp);
+            if (Path.GetExtension(emp.image) == ".png" || Path.GetExtension(emp.image) == ".jpeg" || Path.GetExtension(emp.image) == ".jpg")
+            {
+                var result = SaveData(emp);
+                if (result > 0)
+                {
+                    Label1.Text = "Data Saved Successfully";
+                    using (FileStream fstream = new FileStream(imagepath + fuImage.FileName, FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        fuImage.FileContent.CopyTo(fstream);
+                    }
+                }
+                else
+                {
+                    Label1.Text = "Data Add Fail! may be duplicate data found";
+                }
+                binddata();
+            }
+            else {
+                Label1.Text = "Image Formate is worng! please select correct file";
+            }
+
+        }
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("DeleteEmployee", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@eid", Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString()));
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            binddata();
+        }
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            binddata();
+        }
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int userid = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString());
+            GridViewRow row = (GridViewRow)GridView1.Rows[e.RowIndex];
+            TextBox Name = (TextBox)row.Cells[1].Controls[0];
+            TextBox Email = (TextBox)row.Cells[2].Controls[0];
+            TextBox Dob = (TextBox)row.Cells[3].Controls[0];
+            TextBox image = (TextBox)row.Cells[4].Controls[0];  
+            GridView1.EditIndex = -1;
+            SqlCommand cmd = new SqlCommand("UpdateEmployee", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@eid", userid);
+            cmd.Parameters.AddWithValue("@ename", Name.Text);
+            cmd.Parameters.AddWithValue("@email", Email.Text);
+            cmd.Parameters.AddWithValue("@dob", Convert.ToDateTime(Dob.Text));
+            cmd.Parameters.AddWithValue("@image", image.Text);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            binddata();
+        }
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            binddata();
+        }
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            binddata();
+        }
+
+        private Employee GetDetails(Employee emp)
+        {
+            emp = new Employee();
+            emp.name = txtName.Text;
+            emp.email = txtEmail.Text;
+            emp.dob =Convert.ToDateTime(txtcalnder.Text);
+            emp.image = fuImage.FileName;
+            return emp;
+        }
+
+        public int SaveData(Employee emp) {
+            SqlCommand cmd = new SqlCommand("AddEmployee", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@ename", emp.name);
+            cmd.Parameters.AddWithValue("@email", emp.email);
+            cmd.Parameters.AddWithValue("@dob", emp.dob);
+            cmd.Parameters.AddWithValue("@image", emp.image);
+            conn.Open();
+            var res= cmd.ExecuteNonQuery();
+            conn.Close();
+            
+            return res;
+        }
+        public void binddata()
+        {
+            DataTable dt = new DataTable();
+            SqlCommand cmd = new SqlCommand("GetAllEmployeeDetails", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+           // conn.Close();
+            conn.Open();
+            using (SqlDataAdapter ad = new SqlDataAdapter(cmd)) {
+                ad.Fill(dt);
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            if (dt.Rows.Count <= 1)
+            {
+                //GridView1.Rows[0].Cells.Clear();
+                //GridView1.Rows[0].Cells[0].Text = "record not found";
+            }
+            conn.Close();
+        }
+
+    }
+   
+    public class Employee {
+        public string name { get; set; }
+        public string email { get; set; }
+        public DateTime dob { get; set; }
+        public string image { get; set; }
+    }
+}
